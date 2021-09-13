955a22e8c99325555910b009a236eb4dd76974f7
linhao <1334712251@qq.com>
2021-09-13 20:22:08 +0800

    提交结果

--- src/views/modules/product/attrgroup.vue
+++ src/views/modules/product/attrgroup.vue
@@ -7,7 +7,7 @@
       <div class="mod-config">
         <el-form :inline="true" :model="dataForm" @keyup.enter.native="getDataList()">
           <el-form-item>
-            <el-input v-model="dataForm.key" placeholder="参数名" clearable></el-input>
+            <el-input v-model="dataForm.key" placeholder="id / 组名 / 描述" clearable></el-input>
           </el-form-item>
           <el-form-item>
             <el-button @click="getDataList()">查询</el-button>
